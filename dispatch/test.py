753301857af7f9e67ac42562c0906c48079d553f
rongfudi636 <rongfudi636@pingan.com.cn>
2018-05-17 17:51:22 +0800

    add test

---
+++ dispatch/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'rongfudi636'
+__mtime__ = '2018/5/17'
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
